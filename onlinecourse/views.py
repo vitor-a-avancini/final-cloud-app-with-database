1c5461e2b7e21ba8c04d369e118a59b797173c61
Vitor AVancini <vtravancini@gmail.com>
2022-06-19 06:26:48 +0000

    Final code

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,7 +149,7 @@ def show_exam_result(request, course_id, submission_id):
             grade += answer.question.grade
     context ={
         'course': course,
-        'grade'grade
+        'grade':grade
         }
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
